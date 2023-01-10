f58aad43ba6b51534440565b37a4ecd31508b35b
野村淳也 <unijack@unijackMacMini.local>
2023-01-10 10:08:00 +0900

    fixed2

--- pages/index.js
+++ pages/index.js
@@ -9,33 +9,33 @@ import Introduction from '../components/Introduction'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Footer from '../components/Footer'
 
-import { useEffect, useRef, useState } from 'react'
-import FOG from 'vanta/dist/vanta.fog.min';
-import * as THREE from 'three';
+// import { useEffect, useRef, useState } from 'react'
+// import FOG from 'vanta/dist/vanta.fog.min';
+// import * as THREE from 'three';
 
 export default function Home() {
-  const [vantaEffect, setVantaEffect] = useState(0);
-  const vantaRef = useRef(null);
-  useEffect(() => {
-    if (!vantaEffect) {
-      setVantaEffect(
-        FOG({
-          el: vantaRef.current,
-          THREE,
-          mouseControls: true,
-          touchControls: true,
-          gyroControls: false,
-          minHeight: 200.00,
-          minWidth: 200.00,
-          blurFactor: 0.15,
-          speed: 0.48
-        })
-      )
-    }
-    return () => {
-      if (vantaEffect) vantaEffect.destroy();
-    }
-  }, [vantaEffect]);
+  // const [vantaEffect, setVantaEffect] = useState(0);
+  // const vantaRef = useRef(null);
+  // useEffect(() => {
+  //   if (!vantaEffect) {
+  //     setVantaEffect(
+  //       FOG({
+  //         el: vantaRef.current,
+  //         THREE,
+  //         mouseControls: true,
+  //         touchControls: true,
+  //         gyroControls: false,
+  //         minHeight: 200.00,
+  //         minWidth: 200.00,
+  //         blurFactor: 0.15,
+  //         speed: 0.48
+  //       })
+  //     )
+  //   }
+  //   return () => {
+  //     if (vantaEffect) vantaEffect.destroy();
+  //   }
+  // }, [vantaEffect]);
   return (
     <>
       <Head>
@@ -46,7 +46,9 @@ export default function Home() {
       </Head>
       <div>
         <div id="web-gl"></div>
-        <div ref={vantaRef}>
+        <div 
+          // ref={vantaRef}
+        >
           <main className="text-center max-w-4xl mx-auto px-4">
             <Hero />
             <Introduction />
